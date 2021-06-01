86768ce44ae1d8fe832b9b8ee893ce4eb2d6ff54
zac <isaacmubiru99@gmail.com>
2021-06-01 18:51:51 +0300

    fix(test): improved test coverage

--- test_app.py
+++ test_app.py
@@ -8,12 +8,12 @@ import app
 
 headers = {'Content-Type': 'application/json', 'api-key': '1254'}
 test_data = {
-    "league":  "FA",
-    "home_team": "Arsenal",
-    "away_team": "Man City",
-    "home_team_win_odds": 3.0,
-    "away_team_win_odds": 4,
-    "draw_odds": 2,
+    "league":  "Champions League",
+    "home_team": "Barcelona",
+    "away_team": "PSG",
+    "home_team_win_odds": 2.1,
+    "away_team_win_odds": 3.9,
+    "draw_odds": 6.1,
     "game_date": "18-05-2021"
 }
 
@@ -21,7 +21,6 @@ test_data = {
 @pytest.fixture
 def client():
     db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
-    # app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+app.app.config['DATABASE']
     app.app.config['TESTING'] = True
     client = app.app.test_client()
 
@@ -35,32 +34,34 @@ def client():
 
 
 # test odd creation
-def test_create_without_key(client):
+def test_create_odds_while_not_authenitcated(client):
 
-    rv = client.post('/create', json=test_data)
-    json_data = rv.get_json()
+    resposnse = client.post('/create', json=test_data)
+    json_data = resposnse.get_json()
 
     assert json_data['message'] == 'please contanct admin for api key'
-    assert rv.status_code == 403
+    assert resposnse.status_code == 403
 
 
-def test_create_with_key(client):
-    rv = client.post('/create', headers=headers,
-                     json=test_data)
-    json_data = rv.get_json()
+def test_create_odds_while_authenticated(client):
+    response = client.post('/create', headers=headers,
+                           json=test_data)
+    json_data = response.get_json()
 
-    assert rv.status_code == 200
+    assert response.status_code == 200
+
+# test with wrong data
 
 
-def test_create_with_key_500(client):
+def test_create_odds_with_wrong_data(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
+        "league":  "Champions League",
+        "no_team": "Barcelona",
+        "away_team": "PSG",
         "home_team_win_odds": 3.0,
         "away_team_win_odds": 4,
         "draw_odds": 2,
-        "game_dates_wrong": "2020-02-18"  # wrong data
+        "game_date": "2020-02-18"
     }
     rv = client.post('/create', headers=headers,
                      json=data,)
@@ -71,9 +72,9 @@ def test_create_with_key_500(client):
 # Test update #############################
 def test_update_without_key(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
+        "league":  "Champions League",
+        "home_team": "Barcelona",
+        "away_team": "PSG",
         "home_team_win_odds": 3.0,
         "away_team_win_odds": 7,
         "draw_odds": 2,
@@ -85,39 +86,42 @@ def test_update_without_key(client):
     assert json_data['message'] == 'please contanct admin for api key'
     assert rv.status_code == 403
 
+# Updating odds
 
-def test_update_with_key(client):
+
+def test_update_with_auth(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
-        "home_team_win_odds": 3.0,
-        "away_team_win_odds": 4,
-        "draw_odds": 2,
+        "league":  "Champions League",
+        "home_team": "Barcelona",
+        "away_team": "PSG",
+        "home_team_win_odds": 2,
+        "away_team_win_odds": 5,
+        "draw_odds": 6,
         "game_date": "04-05-2021",
     }
-    rv1 = client.post('/create', headers=headers, json=test_data)  # Create it
-    response1 = rv1.get_json()
-    odds = response1["odds"]
+    response1 = client.post('/create', headers=headers,
+                            json=test_data)  # Create it
+    resData = response1.get_json()
+    odds = resData["odds"]
     odds_id = odds[0]["id"]
     print("the id is", odds_id)
-    assert rv1.status_code == 200
-    rv = client.put(f'/update/{odds_id}', headers=headers,
-                    json=data,)
-    assert rv.status_code == 200
-    rv = client.put('/update/3', headers=headers, json=data)  # non existing
-    assert rv.status_code == 404
+    assert response1.status_code == 200
+    response2 = client.put(f'/update/{odds_id}', headers=headers,
+                           json=data,)
+    assert response2.status_code == 200
+    response2 = client.put('/update/3', headers=headers,
+                           json=data)  # non existing
+    assert response2.status_code == 404
 
 
-def test_update_with_key_500(client):
+def test_update_with_wrong_data(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
-        "home_team_win_odds": 3.0,
-        "away_team_win_odds": 4,
-        "draw_odds": 2,
-        "game_dates_wrong": "01-05-2030"  # error in this parameter
+        "no league ": "Barcelona",
+        "away_team": "PSG",
+        "home_team_win_odds": 5,
+        "away_team_win_odds": 2,
+        "draw_odds": 1.6,
+        "game_dat": "01-05-2030"  # error in this parameter
     }
 
     rv = client.put('/update/1', headers=headers,
@@ -126,78 +130,76 @@ def test_update_with_key_500(client):
     assert rv.status_code == 400
 
 
-# Test   #############################
-def test_read_without_key(client):
+# Test   reading odds
+def test_read_odds_without_auth(client):
     data = {
-        "league":  "FA",
-        "date_range": "12-06-2015 to 15-06-2025"
+        "league":  "Champions League",
+        "date_range": "12-09-2018 to 18-09-2024"
     }
-    rv = client.get('/read', json=data)
-    json_data = rv.get_json()
+    response = client.get('/read', json=data)
+    json_data = response.get_json()
 
     assert json_data['message'] == 'please contanct admin for api key'
-    assert rv.status_code == 403
+    assert response.status_code == 403
 
 
-def test_read_with_key(client):
+def test_read_odds_with_auth(client):
     data = {
-        "league": "FA",
-        "date_range": "12-06-2015 to 15-06-2025"
+        "league": "Champions League",
+        "date_range": "12-09-2018 to 18-09-2024"
     }
-    rv = client.get('/read', json=data, headers=headers)
-    json_data = rv.get_json()
-    assert rv.status_code == 200
+    response = client.get('/read', json=data, headers=headers)
+    json_data = response.get_json()
+    assert response.status_code == 200
 
 
-def test_read_with_key_500(client):
+def test_read_odds_with_invalid_filds(client):
     data = {
-        "league": "FA",
-        "date_ranges_WRONG": "12-06-2015 to 15-06-2025"  # wrong param
+        "league": "Champions League",
+        "invalid": "112-09-2018 to 18-09-2024"  # wrong param
     }
-    rv = client.get('/read', json=data, headers=headers)
-    json_data = rv.get_json()
-    assert rv.status_code == 400
+    response = client.get('/read', json=data, headers=headers)
+    json_data = response.get_json()
+    assert response.status_code == 400
 
 
 # Test Delete
-def test_delete_without_key(client):
+def test_delete_odds_with_no_auth(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
+        "league":  "Champions League",
+        "home_team": "Barcelona",
+        "away_team": "PSG",
         "game_date": "15-06-2025"
     }
 
-    rv = client.delete('/delete', json=test_data)
-    json_data = rv.get_json()
+    response = client.delete('/delete', json=test_data)
+    json_data = response.get_json()
 
     assert json_data['message'] == 'please contanct admin for api key'
-    assert rv.status_code == 403
+    assert response.status_code == 403
 
 
-def test_delete_with_key(client):
+def test_delete_with_auth(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
+        "league":  "Champions League",
+        "home_team": "Barcelona",
+        "away_team": "PSG",
         "game_date": "18-05-2021"
 
     }
-    rv1 = client.post('/create', headers=headers, json=test_data)  # Create it
-    print("response1 ",rv1.get_json())
-    assert rv1.status_code == 200
-    rv = client.delete('/delete', json=data, headers=headers)  # Delete it
-    print("response2 ",rv.get_json())
-
-  
+    response1 = client.post('/create', headers=headers, json=test_data)  # Create it
+    print("response1 ", response1.get_json())
+    assert response1.status_code == 200
+    response = client.delete('/delete', json=data, headers=headers)  # Delete it
+    print("response2 ", response.get_json())
 
 
-def test_delete_with_key_500(client):
+def test_delete_with_invalid_data(client):
     data = {
-        "league":  "FA",
-        "home_team": "Arsenal",
-        "away_team": "Man City",
-        "game_date_wrong": "215-06-2025"  # wrong param
+        "league":  "Champions League",
+        "no_team": "Barcelona",
+        "away_team": "PSG",
+        "game_date": "215-06-2025"  # wrong param
     }
-    rv = client.delete('/delete', json=data, headers=headers)
-    assert rv.status_code == 400
+    response = client.delete('/delete', json=data, headers=headers)
+    assert response.status_code == 400
